e63b8ef3b42cf31dcab822953c3581b7056e6d83
Zechariah <zechariahtan144@gmail.com>
2022-05-04 22:11:53 +0800

    :sparkles: add extra logging to server

--- src/app.ts
+++ src/app.ts
@@ -16,3 +16,63 @@ const server = new NodeMediaServer({
 })
 
 server.run()
+
+server.on("preConnect", (id, args) => {
+	console.log("[NodeEvent on preConnect]", `id=${id} args=${JSON.stringify(args)}`)
+	// let session = server.getSession(id);
+	// session.reject();
+})
+
+server.on("postConnect", (id, args) => {
+	console.log("[NodeEvent on postConnect]", `id=${id} args=${JSON.stringify(args)}`)
+})
+
+server.on("doneConnect", (id, args) => {
+	console.log("[NodeEvent on doneConnect]", `id=${id} args=${JSON.stringify(args)}`)
+})
+
+server.on("prePublish", (id, StreamPath, args) => {
+	console.log(
+		"[NodeEvent on prePublish]",
+		`id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+	)
+	// let session = server.getSession(id);
+	// session.reject();
+})
+
+server.on("postPublish", (id, StreamPath, args) => {
+	console.log(
+		"[NodeEvent on postPublish]",
+		`id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+	)
+})
+
+server.on("donePublish", (id, StreamPath, args) => {
+	console.log(
+		"[NodeEvent on donePublish]",
+		`id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+	)
+})
+
+server.on("prePlay", (id, StreamPath, args) => {
+	console.log(
+		"[NodeEvent on prePlay]",
+		`id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+	)
+	// let session = server.getSession(id);
+	// session.reject();
+})
+
+server.on("postPlay", (id, StreamPath, args) => {
+	console.log(
+		"[NodeEvent on postPlay]",
+		`id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+	)
+})
+
+server.on("donePlay", (id, StreamPath, args) => {
+	console.log(
+		"[NodeEvent on donePlay]",
+		`id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`
+	)
+})
